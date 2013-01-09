b02c41b16acc699c8ff781c6f3c636691fec4d62
Ting Wang <tting.wang@gmail.com>
2013-01-09 18:06:25 +0800

    Add confirm dialog before deleting product.

--- js-src/app/scripts/controllers/main.js
+++ js-src/app/scripts/controllers/main.js
@@ -4,15 +4,25 @@ jsSrcApp.controller('MainCtrl', ['$scope', 'product', function($scope, product) 
   $scope.products = product.Product.query();
 
   $scope.product = new product.Product;
-  var scope = $scope;
-
+  console.log('ini confirm delete to false');
+  $scope.confirmDelete = false;
   $scope.delete = function(id) {
-    var product = scope.product;
-
-    console.log(product);
-
-    product.$delete({id: id}, function(product) {
-        console.log(product.title);
-    });
+      var product = $scope.product;
+      console.log($scope.confirmDelete);
+      if ($scope.confirmDelete) {
+          console.log('try to delete product');
+          product.$delete({id: id}, function(product) {
+              var index = $scope.products.indexOf(product);
+              for (var i = 0; i < $scope.products.length; i++) {
+                  if (id == $scope.products[i]._id) {
+                      $scope.products.splice(i, 1);
+                  }
+              }
+              //$scope.confirmDelete = false;
+              //console.log($scope.products);
+          });
+      } else {
+          $scope.confirmDelete = true;
+      }
   };
 }]);
--- js-src/app/scripts/directives/effect.js
+++ js-src/app/scripts/directives/effect.js
@@ -14,4 +14,21 @@ jsSrcApp.directive('fadey', function() {
         });
     }
   };
+}).directive('buttonDelete', function($compile, $rootScope) {
+    return {
+        restrict: 'E',
+        replace: true,
+        template: '<a class="button-delete"><i style="margin-top: 4px;" class="icon-remove"></i></a>',
+        link: function(scope, element, attrs) {
+        //debugger;
+          console.log(element);
+            $(element).clickover({
+                global: true,
+                title: 'Are you sure?',
+                content: "<div id='my-poppy' class='btn-toolbar'><button id='button-confirm-delete-cancel' data-dismiss='clickover' class='btn'>Cancel</button><button id='button-confirm-delete-ok' class='btn btn-danger' ng-click='destroy(v)'>Delete</button></div>"
+            }).on('shown', function() {
+                $compile($("#my-poppy").contents())(scope);
+            });
+        }
+    }
 });
