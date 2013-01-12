a744db57ba66a342b1b948cdb15140f3c299d6e3
Ting Wang <tting.wang@gmail.com>
2013-01-12 12:04:10 +0800

    Abstract db into one class.

--- js-src/mocks-data/product.js
+++ js-src/mocks-data/product.js
@@ -1,12 +1,7 @@
-var mongodb = require('mongodb');
-var format = require('util').format;
-var Db = mongodb.Db;
-var Connection = mongodb.Connection;
-var Server = mongodb.Server;
-var BSON = mongodb.BSONPure;
+var mdb = require(__dirname + '/mdb.js');
 
-var server = new Server('localhost', 27017, {auto_reconnect: true});
-db = new Db('crm', server);
+var server = new mdb.Server('localhost', 27017, {auto_reconnect: true});
+db = new mdb.Db('crm', server);
  
 db.open(function(err, db) {
     if(!err) {
@@ -20,86 +15,9 @@ db.open(function(err, db) {
     }
 });
 
-exports.findById = function(req, res) {
-    var id = req.params.id;
-    console.log('Retrieving product: ' + id);
-    db.collection('products', function(err, collection) {
-        if (err) {
-
-        } else {
-            collection.findOne({'_id':new BSON.ObjectID(id)}, function(err, item) {
-                res.send(item);
-            });
-        }
-    });
-};
-
-exports.findAll = function(req, res) {
-    db.collection('products', function(err, collection) {
-        if (err) {
-            res.status(500);
-            res.end;
-        } else {
-            collection.find().toArray(function(err, items) {
-                res.status(200); 
-                res.send(items);
-            });
-        }
-    });
-};
-
-exports.addProduct = function(req, res) {
-    var product = req.body;
-    db.collection('products', function(err, collection) {
-        collection.insert(product, {safe: true}, function(err, result) {
-            if (err) {
-                res.status(500);
-                res.end();
-            } else {
-                res.status(201);
-                res.set('Location', '/products/' + product._id);
-                res.end();
-            }
-        });
-    });
-}
-
-exports.updateProduct = function(req, res) {
-    var id = req.params.id;
-    var product = req.body;
-    product._id = new BSON.ObjectID(product._id);
-    db.collection('products', function(err, collection) {
-        if (err) {
-            console.log(err);
-            res.status(500);
-            res.end();
-        } else {
-            //@todo: check if the content is there, if not return 204
-            collection.update({'_id': new BSON.ObjectID(id)}, product, {safe: true}, function(err, result) {
-                if (err) {
-                    console.log(err);
-                    res.status(500);
-                    res.end();
-                } else {
-                    res.status(200);
-                    res.send(product);
-                }
-            });
-        }
-    });
-}
-
-exports.deleteProduct = function(req, res) {
-    var id = req.params.id;
-    db.collection('products', function(err, collection) {
-        collection.remove({'_id': new BSON.ObjectID(id)}, {safe: true}, function(err, result) {
-            if (err) {
-                res.status(500);
-                res.end();
-            } else {
-                res.status(200);
-                res.end();
-            }
-        });
-    });
-}
+var dbMapper = new mdb.DbMapper(db, 'products');
+exports.findById = dbMapper.findById;
+exports.findAll = dbMapper.findAll;
+exports.addProduct = dbMapper.add;
+exports.updateProduct = dbMapper.update;
+exports.deleteProduct = dbMapper.delete;
--- js-src/restifyServer.js
+++ js-src/restifyServer.js
@@ -6,51 +6,15 @@ var url = require('url');
 var product = require(__dirname + '/mocks-data/product.js');
 
 // test data base dir
-var testDir = __dirname + '/mocks-data';
+// var testDir = __dirname + '/mocks-data';
 var app = express();
 app.use(express.bodyParser());
+app.use(express.static(path.join(__dirname, 'app')));
 
 app.get('/products', product.findAll);
-//app.get('/products', function(req, res) {
-    //res.status('200');
-    //res.setHeader('Content-Type', 'application/json');
-    //res.end('[{"id": 1, "name": "iphone"}]');
-//});
-
-function mockHandler(fileName, accept, res) {
-    var fullPath = testDir + '/' + fileName;
-    var accepts = accept.split(',');
-    if (accepts[0] === 'application/json') {
-        fullPath = testDir + '/' + fileName + '.json';
-    }
-    fs.exists(fullPath, function(exists) {
-        if (exists) {
-            console.log('exists');
-            fs.readFile(fullPath, function(err, content) {
-                if (err) {
-                    res.status(500);
-                    res.end();
-                } else {
-                    console.log(content);
-                    res.status(200);
-                    res.end(content);
-                }
-            });
-        } else {
-            console.log([fullPath + ' donesn\'t exists', accept]);
-            res.status(500);
-        }
-    });
-}
-
 app.get('/products/:id', product.findById);
-
 app.post('/products', product.addProduct);
-
 app.put('/products/:id', product.updateProduct);
-
 app.delete('/products/:id', product.deleteProduct);
-
-app.use(express.static(path.join(__dirname, 'app')));
 
 app.listen(3030);
